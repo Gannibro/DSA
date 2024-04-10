9c9206fc89818cd505dd3f9a70b0b11c576937d5
Aryaman Giri <praksatyal@gmail.com>
2024-04-10 05:09:19 +0100

    First Commit

---
+++ linkedlist.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#define linkedlist_h
+
+class Node{
+    public:
+        int info;
+        Node * next;
+
+        Node(){}
+        Node(int d) : info (d), next(NULL) {}
+        Node(int d, Node * next) : info(d), next(next) {}
+};
