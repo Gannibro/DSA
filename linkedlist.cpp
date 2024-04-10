127feb3e6dc0cb94f64d8efa8940b354ba6eafee
Aryaman Giri <aryamankgiri@gmail.com>
2024-04-10 05:47:37 +0100

    Header and CPP Files

---
+++ linkedlist.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "linkedlist.h"
+using namespace std;
+
+LinkedList::LinkedList(){
+    this -> HEAD = NULL;
+    this -> TAIL = NULL;
+}
+
+bool LinkedList::isEmpty(){
+    return HEAD == NULL;
+}
+
+void LinkedList::addToHead(int data){
+    Node * newNode = new Node(data, HEAD);
+    if (this -> isEmpty()){
+        TAIL = newNode;
+    }
+    HEAD = newNode;
+}
+
+void LinkedList::print(char separator){
+    cout << "Contents: " << endl;
+    for (Node* t = HEAD; t != NULL; t = t->next){
+        cout << t->info << separator;
+    }
+}
