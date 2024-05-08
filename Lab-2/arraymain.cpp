09d24f861b079237a514f1965fe15571fd593f07
Aryaman Giri <aryamankgiri@gmail.com>
2024-05-08 06:09:07 +0100

    Main File added

---
+++ Lab-2/arraymain.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "arraystack.h"
+using namespace std;
+
+int main(){
+    Stack *s = new ArrayStack(10);
+    s->push(10);
+    s->push(9);
+
+    int element;
+
+    cout << s->top(element) << endl;
+    cout << element << endl;
+    cout << s->pop(element) << endl;
+    cout << element << endl;
+    cout << s->pop(element) << endl;
+    cout << element << endl;
+    cout << s->pop(element) << endl;
+}
