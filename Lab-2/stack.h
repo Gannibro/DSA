73bede930efbeedd49bfd7ed7b6c955be0d683b0
Aryaman Giri <aryamankgiri@gmail.com>
2024-05-23 19:07:44 +0545

    Removed old files

--- Lab-2/stack.h
+++
@@ -1,9 +0,0 @@
-class Stack{
-    public:
-        Stack(){}
-        ~Stack(){}
-            virtual void push(int) = 0;
-            virtual int pop() = 0;
-            virtual bool isEmpty() = 0;
-            virtual int top() = 0;
-};
